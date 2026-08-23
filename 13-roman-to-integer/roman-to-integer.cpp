79c18611d3484b7f88697f2bb3c1cbf73d255d27
Riddhi743 <riddhigandhi1111@gmail.com>
2026-08-24 00:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (94.54%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int romanToInt(string s) {
-       char arr1[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-        int arr2[] = {1, 5, 10, 50, 100, 500, 1000};
-
+        char arr1[] = {'I','V','X','L','C','D','M'};
+        int arr2[]={1,5,10,50,100,500,1000};
         int ans=0;
         for(int i=0;i<s.length();i++){
             int curr=0;
             int nex=0;
-            for(int j=0;j<7;j++ ){
+            for(int j=0;j<7;j++){
                 if(s[i]==arr1[j]){
                     curr=arr2[j];
                     break;
                 }
             }
-            if(i+1<s.length()){
-                for(int j=0;j<7;j++ ){
-                if(s[i+1]==arr1[j]){
-                    nex=arr2[j];
-                    break;
+            if(i+1 <s.length()){
+                for(int j=0;j<7;j++){
+                    if(s[i+1]==arr1[j]){
+                        nex=arr2[j];
+                        break;
+                    }
                 }
             }
-            }
-            if(curr<nex)    ans-=curr;
-            else  ans+=curr;
+            if(curr<nex)
+                ans-=curr;
+            else
+                ans+=curr;
         }return ans;
     }
+
 };
